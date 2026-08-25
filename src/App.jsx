a1794b953f571af0c361861e95255f6338e5f2d4
bebsiizcool <basilshahzad18@gmail.com>
2026-08-25 17:31:53 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from './page/Hero'
+const App = () => {
+  return (
+    <div>
+      <div className='bg-[conic-gradient(from_180deg_at_bottom_right,_#020617_0%,_#0C4A6E_60%)] text-white h-screen'>
+        <Hero/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/page/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="flex justify-center p-12">
+        <h1 className='text-5xl font-medium '>Create Your Todos</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
