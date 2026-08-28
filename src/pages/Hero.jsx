bd3b5d8dd898293ce48ef3c77163a7916b496194
bebsiizcool <basilshahzad18@gmail.com>
2026-08-28 17:33:57 +0500

    learning

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -2,41 +2,68 @@ import React from 'react'
 import { useState } from 'react'
 const Hero = () => {
 
-  const [todo, settodo] = useState("")
-  const [todos, settodos] = useState([])
+  const [todo, settodo] = useState("")    
+  const [maintask, setMaintask] = useState([])
 
-  const addtodo = () => {
-    settodos([
-      ...todos, {
-        id: Date.now(),
-        text: todo,
-        completed: false
-      }
-    ])
+const deletehandler = (i)=>{
+    let copytask  = [...maintask]
+    copytask.splice(i)
+      setMaintask(copytask)
+    
+}
 
+  let rendertask = <h1> No Task Available</h1>
+  
+ if(maintask.length>0){
+   rendertask = maintask.map((t,i) =>{
+    return(
+      <li key={i} className='flex justify-between items-center'>
+      <div>
+        <h5>{t.todo}</h5>
+      </div>
+      <button onClick={()=>{
+        deletehandler(i)
+      }}>delete</button>
+      </li>
+    )
+  })
+ }
+
+
+ const onsubmit = (e)=>{
+    e.preventDefault()
+    setMaintask([...maintask,{todo}])
     settodo("")
-  }
+    console.log(maintask)
+ }
+
   return (
     <div>
       <div className="flex flex-col items-center justify-center p-12">
         <h1 className='text-5xl font-medium '>Create Your Todos</h1>
         <div >
-          <input className='bg-amber-50 relative text-gray-500 w-3xl mt-5 p-3' type="text" placeholder='Enter your Todos' value={todo}
-            onChange={(e) => settodo(e.target.value)} />
-          <button onClick={addtodo} className='p-3 bg-blue-900 hover:cursor-pointer'>ADD</button>
+          <form onSubmit={onsubmit}>
+          <input className='bg-amber-50 relative text-gray-500 w-3xl mt-5 p-3'
+           type="text" 
+           placeholder='Enter your Todos' 
+           value={todo} 
+           onChange={(e) => settodo(e.target.value)}
+            />
+
+          <button className='p-3 bg-blue-900 hover:cursor-pointer'>ADD</button>
+          </form>
         </div>
 
-        <div className=' bg-blue-300 w-4xl flex  mt-5'>
+        <div className=' bg-blue-300 w-4xl flex flex-col mt-5'>
           <h1 className='text-2xl p-4'>Your Todos</h1>
-          <div>
-  {todos.map((item) => (
-    <div key={item.id}>
-      <span>{item.text}</span>
 
-      
-    </div>
-  ))}
-</div>
+            <ul>
+          {rendertask}
+          </ul>
+          <div>
+          
+
+          </div>
         </div>
       </div>
 
